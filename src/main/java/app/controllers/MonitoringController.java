7d1abc6bec6fc8d200c400628f8a7c99f6948ef9
greepz <trunov.a.serg@sberbank.ru>
2019-10-24 14:41:59 +0300

    add service

--- src/main/java/app/controllers/MonitoringController.java
+++ src/main/java/app/controllers/MonitoringController.java
@@ -3,9 +3,12 @@ package app.controllers;
 
 import app.controllers.dto.monitoring.MonitoringDto;
 import app.controllers.dto.monitoring.ReportDto;
+import app.services.AnalyzerService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +24,10 @@ import java.util.List;
 @RestController
 public class MonitoringController {
 
+    @Autowired
+    @Qualifier("alalyzeService")
+    private AnalyzerService service;
+
     @PostMapping("/monitoring/send")
 
     public ResponseEntity<String> receive(@RequestBody MonitoringDto monitoringDto) throws JsonProcessingException {
@@ -29,17 +36,17 @@ public class MonitoringController {
     }
 
     @GetMapping("/monitoring/get")
-    public ResponseEntity<List<ReportDto>> getReport(@RequestParam(name = "page", required = false) int page,
+    public ResponseEntity<List<ReportDto>> getReport(@RequestParam(name = "page", required = false, defaultValue = "0") int page,
+                                                     @RequestParam(name = "size", required = false, defaultValue = "20") int size,
                                                      @RequestParam(name = "sortField", required = false) String sortField,
                                                      @RequestParam(name = "sortOrder", required = false) String sortOrder,
                                                      @RequestParam(name = "filterField", required = false) String filterField,
                                                      @RequestParam(name = "filterValue", required = false) String filterValue){
 
+        List<ReportDto> list = service.getReport(page, size, sortField, sortOrder, filterField, filterValue);
 
-
-        return new ResponseEntity<>(new LinkedList<>(), HttpStatus.OK);
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
-
 
 }
 
--- src/main/java/app/services/impl/AndroidAnalyzerService.java
+++ src/main/java/app/services/impl/AndroidAnalyzerService.java
@@ -3,11 +3,23 @@ package app.services.impl;
 import app.controllers.dto.monitoring.MonitoringDto;
 import app.controllers.dto.monitoring.ReportDto;
 import app.converters.ReportMapper;
+import app.repository.ReportsRepository;
 import app.services.AnalyzerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 
-
+@Component("alalyzeService")
 public class AndroidAnalyzerService implements AnalyzerService {
+
+    @Autowired
+    ReportsRepository repository;
 
     @Override
     public void save(MonitoringDto monitoringDto) {
@@ -16,6 +28,10 @@ public class AndroidAnalyzerService implements AnalyzerService {
 
     @Override
     public List<ReportDto> getReport(int page, int size, String sortField, String sortOrder, String filterField, String filterValue) {
-        return ReportMapper.generate(new ArrayList<Report>());
+
+        Sort sort = new Sort(Direction.fromString(sortOrder), sortField);
+        Pageable pageable = PageRequest.of(page, size, sort);
+        return ReportMapper.generate(repository.findAll(pageable).getContent());
+
     }
 }
