2573a9ab37776dac0e36ae991aad0354c5eac975
greepz <trunov.a.serg@sberbank.ru>
2019-10-25 07:24:19 +0300

    hz commit

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,6 +2,10 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Main {
@@ -9,5 +13,6 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
+
 
 }
--- src/main/java/app/SpringSecurityConfig.java
+++ src/main/java/app/SpringSecurityConfig.java
@@ -5,37 +5,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-@EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private AuthenticationEntryPoint authEntryPoint;
+    // Create 2 users for demo
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
+        auth.inMemoryAuthentication()
+                .withUser("Trunov").password("{noop}password").roles("USER")
+                .and()
+                .withUser("Pleshkanov").password("{noop}password").roles("USER");
+
+    }
+
+    // Secure the endpoins with HTTP Basic authentication
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
-                .anyRequest().authenticated()
-                .and().httpBasic()
-                .authenticationEntryPoint(authEntryPoint);
-    }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("Trunov").password(encoder().encode("password")).roles("USER")
-                .and().withUser("Gorshkov").password(encoder().encode("password")).roles("USER")
-                .and().withUser("Pleshkanov").password(encoder().encode("password")).roles("USER")
-                .and().withUser("Burgasov").password(encoder().encode("password")).roles("USER");
-    }
-
-    @Bean
-    public BCryptPasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
+        http
+                //HTTP Basic authentication
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers( "/monitoring/**").hasRole("USER")
+                .antMatchers( "/iperf/**").hasRole("USER")
+               .and()
+                .csrf().disable()
+                .formLogin().disable();
     }
 
 }
